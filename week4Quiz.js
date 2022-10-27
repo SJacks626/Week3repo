d8d88289e794a4471e7227229992e38e93d3e653
Sarandë Jackson <114336230+SJacks626@users.noreply.github.com>
2022-10-27 00:18:50 -0400

    homework

---
+++ week4Quiz.js
@@ -0,0 +1,55 @@
+test.js131 Sam
+test.js:132 Phoenix
+test.js:136 Sam
+test.js:142 Array(28)
+test.js:147 Object
+test.js:131 Carl
+test.js:132 Peoria
+test.js:136 Carl
+test.js:142 Array(28)
+test.js:147 false
+test.js:131 Kari
+test.js:132 Glendale
+test.js:136 Kari
+test.js:147 Object
+test.js:131 Carissa
+test.js:132 Phoenix
+test.js:136 Carissa
+test.js:142 Array(28)
+test.js:147 Object
+test.js:131 Chris
+test.js:132 Phoenix
+test.js:136 Chris
+test.js:142 Array(28)
+test.js:147 false
+test.js:131 Marissa
+test.js:132 PEORIA
+test.js:136 Marissa
+test.js:142 Array(28)
+test.js:147 Object
+test.js:131 Courtney
+test.js:132 Phoenix
+test.js:136 Courtney
+test.js:142 Array(28)
+test.js:147 false
+test.js:131 Carissa
+test.js:132 Phoenix
+test.js:136 Carissa
+test.js:147 Object
+test.js:131 Larissa
+test.js:132 Phoenix
+test.js:136 Larissa
+test.js:147 Object
+test.js:131 charlie
+test.js:132 Peoria
+test.js:136 charlie
+test.js:142 Array(28)
+test.js:147 false
+test.js:131 CHARISARD
+test.js:132 Pokemon
+test.js:136 CHARISARD
+test.js:142 Array(28)0: {name: 'Sam', address: {…}, membershipLevel: 'GOLD', age: 32}1: false2: {name: 'Carl', address: {…}, 
+membershipLevel: 'PLATINUM', age: 28}3: {name: 'Carl', address: {…}, membershipLevel: 'PLATINUM', age: 28}4: 
+false5: {name: 'Kari', address: {…}, membershipLevel: 'GOLD', age: 40}6: false7: {name: 'Carissa', address: {…}, membershipLevel: 'SILVER', age: 28}8: {name: 'Carissa', address: {…}, membershipLevel: 'SILVER', age: 28}9: false10: {name: 'Chris', address: {…}, membershipLevel: 'PLATINNUM', age: 28}11: {name: 'Chris', address: {…}, membershipLevel: 'PLATINNUM', age: 28}12: false13: {name: 'Marissa', address: {…}, membershipLevel: 'SILVER', age: 30}14: false15: {name: 'Courtney', address: {…}, membershipLevel: 'GOLD', age: 25}16: {name: 'Courtney', address: {…}, membershipLevel: 'GOLD', age: 25}17: false18: {name: 'Carissa', address: {…}, membershipLevel: 'SILVER', age: 28}19: false20: {name: 'Larissa', address: {…}, membershipLevel: 'SILVER', age: 28}21: false22: {name: 'charlie', address: {…}, membershipLevel: 'SILVER', age: 29}23: {name: 'charlie', address: {…}, membershipLevel: 'SILVER', age: 29}24: false25: {name: 'CHARISARD', address: {…}, membershipLevel: 'SILVER', age: 15}26: {name: 'CHARISARD', address: {…}, membershipLevel: 'SILVER', age: 15}27: falselength: 28[[Prototype]]: Array(0)
+test.js:147 Objectaddress: {street: '1172 W Thunderbird Rd', city: 'Phoenix', zip: '85308', state: 'AZ'}age:
+ 28membershipLevel: "PLATINNUM"name: "Chris"[[Prototype]]: Object
